b6308160cd95aa60ffc36beab533262fdb02580c
lroman-p <145353783+lroman-p@users.noreply.github.com>
2023-11-21 19:20:32 +0100

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -28,3 +28,23 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	dst[j] = '\0';
 	return (dlen + ft_strlen(src));
 }
+*//size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+	size_t	aux;
+
+	if (!dst && dstsize == 0)
+		return (0);
+	aux = ft_strlen(dst);
+	if (dstsize <= ft_strlen(dst))
+		return (dstsize + ft_strlen(src));
+	i = 0;
+	j = ft_strlen(dst);
+	while (src[i] && j + 1 < dstsize)
+	{
+		dst[j++] = src[i++];
+	}
+	dst[j] = '\0';
+	return (aux + ft_strlen(src));
+} //*
