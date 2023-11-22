9528db675757ad835a4c268a36e9436b63fabb0d
lroman-p <145353783+lroman-p@users.noreply.github.com>
2023-11-22 10:49:30 +0100

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -14,35 +14,6 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	if (!*needle)
-		return ((char *)haystack);
-	while (haystack[i] != '\0' && i < len)
-	{
-		if (haystack[i] == needle[0])
-		{
-			j = 0;
-			while ((needle[j] != '\0'
-					&& haystack [i + j] == needle[j] && (i + j) < len))
-			{
-				if (needle[j + 1] == '\0')
-					return ((char *) haystack + i);
-				j++;
-			}
-		}
-		i++;
-	}
-	return (NULL);
-}
-
-*//
-#include "libft.h"
-
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
-{
 	size_t	h;
 	size_t	j;
 
@@ -68,4 +39,3 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	}
 	return (NULL);
 }
-//*
