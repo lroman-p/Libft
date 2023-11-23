d377c8eb944e2ddf2e82ad7a7b073ff553514795
lroman-p <145353783+lroman-p@users.noreply.github.com>
2023-11-23 12:10:23 +0100

    Update ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -20,3 +20,28 @@ void	ft_bzero(void	*s, size_t n)
 	while (n--)
 		*str++ = '\0';
 }
+//
+
+#include "libft.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+
+void	ft_bzero(void	*s, size_t n)
+{
+	unsigned char	*str;
+
+	str = (unsigned char *)s;
+	while (n--)
+		*str++ = '\0';
+}
+
+int	main()
+{
+	char	str[] = "42madrid";
+
+	ft_bzero(str, ;
+	write(1, str, sizeof(str) - 1);
+	return (0);
+}
+//
